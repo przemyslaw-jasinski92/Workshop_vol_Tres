592a4528a08d187ea1721889cf7fbdac3f78f01f
przemyslaw-jasinski92 <>
2022-10-12 15:55:48 +0200

    v.1.0 working

--- cinemas_ex/tickets.sql
+++
@@ -1,9 +0,0 @@
-create table tickets
-(
-    id       int auto_increment
-        primary key,
-    quantity int            null,
-    price    decimal(10, 2) null,
-    status   int default 0  null
-);
-
